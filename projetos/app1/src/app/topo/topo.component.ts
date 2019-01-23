6dfa75dfbd9d32b89f039ef9c6d555a55dd7fcaf
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2019-01-23 17:37:41 -0200

    Adding top component, and form to the learn english app

---
+++ projetos/app1/src/app/topo/topo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+// Adding decorator encaminha metadata
+@Component({
+    selector: 'app-topo',
+    templateUrl: './topo.component.html'
+})
+export class TopoComponent {}
--- projetos/app1/src/assets/arquivos_da_aula/app/app.module.ts
+++ projetos/app1/src/assets/arquivos_da_aula/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { PainelComponent } from './painel/painel.component';
@@ -17,8 +16,7 @@ import { ProgressoComponent } from './progresso/progresso.component'
     ProgressoComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
